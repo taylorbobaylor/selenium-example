c61e7478fce7ca9676fb36f73180d80e0af207b8
Taylor-Christian_LPLA <taylorbobaylor@gmail.com>
2022-08-10 23:35:27 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["Selenium/Selenium.csproj", "Selenium/"]
+RUN dotnet restore "Selenium/Selenium.csproj"
+COPY . .
+WORKDIR "/src/Selenium"
+RUN dotnet build "Selenium.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "Selenium.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Selenium.dll"]
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+while (true)
+{
+    Console.WriteLine("Run selenium automation? ");
+    var res = Console.ReadLine();
+
+    if (res?.ToLowerInvariant() == "y" || res?.ToLowerInvariant() == "yes")
+        StartSeleniumAutomation();
+    else
+        return;
+}
+
+
+async Task StartSeleniumAutomation()
+{
+    var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+
+    driver.Manage().Window.Maximize();
+    driver.Url = "https://www.google.com";
+    driver.Navigate();
+
+    var searchBox = driver.FindElement(By.Name("q"));
+    searchBox.SendKeys("how to run selenium tests in c#");
+    searchBox.Submit();
+    
+    await Task.Delay(TimeSpan.FromSeconds(3));
+    driver.Quit();
+}
